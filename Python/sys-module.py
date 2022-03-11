e0a05d2880b6eca5cfbc2a69fade7f89f6e89ec7
Suman1107 <moharanasuman124@gmail.com>
2022-03-11 18:08:57 +0530

    "first"

---
+++ Python/sys-module.py
@@ -0,0 +1,12 @@
+import sys
+import os
+print(sys.argv)
+print(os.sep)
+print(os.getcwd()+os.sep)
+os.chdir("C:\\Users\\SU20307312\\Desktop")
+print(os.getcwd())
+print(os.listdir("C:\\Users\\SU20307312\\Desktop"))
+#os.mkdir("python-test")
+#print(os.listdir("C:\\Users\\SU20307312\\Desktop"))
+os.makedirs("python-test\\new\\test")
+print(os.listdir("C:\\Users\\SU20307312\\Desktop\\python-test"))
