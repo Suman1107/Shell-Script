106c54536e0bfcec6759d9b15ac673980ccd85ba
Suman1107 <moharanasuman124@gmail.com>
2022-04-04 13:03:54 +0530

    New Code

---
+++ Shell-Scripting/basic-tash.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+#Author: Suman
+#Purpose: Running basic task 
+#Date: 04-04-22
+
+echo
+echo This script will run below basic tasks sequentially
+echo
+pwd
+echo
+ls
+echo
+whoami
+echo
+Date
+echo
+cal
+echo
+touch a b c
+echo 
+echo End Of Script.
+
