0945c73f56bc3c691b4436d63ca9b572183ed931
Suman1107 <moharanasuman124@gmail.com>
2022-04-04 13:21:39 +0530

    Added basic-adminstration-task.sh

---
+++ Shell-Scripting/basic-admin-task.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#Author: Suman
+#Purpose: Running basic adminstration task 
+#Date: 04-04-22
+
+echo
+echo This script will run basic Adminstration task
+echo
+top | head 10
+echo
+df -h
+echo
+free -h
+echo
+uptime
+echo
+iostat
+echo
+echo End of Script.
