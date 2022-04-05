30b9176b7ff126073dbb564c107d8ce85cebc686
Suman1107 <moharanasuman124@gmail.com>
2022-04-05 12:14:19 +0530

    Added input-output.sh

---
+++ Shell-Scripting/input-output.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#Author: Suman
+#Purpose: Dealing with Input & Output  
+#Date: 04-04-22
+
+echo Hello, My name is Suman
+echo
+echo What is your name?
+read yourname
+echo
+echo Hello $yourname
