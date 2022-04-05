853c96851e78637968d557c3bd2ad1a7ab0422a4
Suman1107 <moharanasuman124@gmail.com>
2022-04-05 15:34:43 +0530

    Added If-else.sh

---
+++ Shell-Scripting/if-else.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#Author: Suman
+#Purpose: If-Else Condition
+#Date: 04-04-22
+
+clear
+echo
+echo "What is your name?"
+read name
+echo "Hello $name , Do you work in IT ? (y/n)"
+read like
+If ["$like" = y]
+then echo "Cool, you are at the right track"
+elif ["$like" = n]
+then echo "You should try it, It's really cool"
+fi
+
