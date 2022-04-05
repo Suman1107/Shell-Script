ed0a9201541cbe6f9d46031933c9da0c570853ad
Suman1107 <moharanasuman124@gmail.com>
2022-04-05 12:25:52 +0530

    Updated input-output.sh

--- Shell-Scripting/input-output.sh
+++ Shell-Scripting/input-output.sh
@@ -3,9 +3,8 @@
 #Purpose: Dealing with Input & Output  
 #Date: 04-04-22
 
-echo Hello, My name is Suman
+machine-name=`hostname` #these `` are mandatory if you want to run the value between those `` as a command 
+echo "Hello User, What's your name?"
+read user
 echo
-echo What is your name?
-read yourname
-echo
-echo Hello $yourname
+echo Hello $user Your machine name is $machine-name
