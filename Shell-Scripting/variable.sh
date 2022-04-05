9693ab29dc0c70e4a17ad52fef62951ad3cae465
Suman1107 <moharanasuman124@gmail.com>
2022-04-05 11:51:48 +0530

    Added Variable.sh

---
+++ Shell-Scripting/variable.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#Author: Suman
+#Purpose: Declaring and Running Variables 
+#Date: 05-04-22
+
+p=pwd
+l=ls-l
+wi=whoami
+c= "cal 2022"
+
+echo Running Variables
+echo
+$p
+echo
+$l
+echo
+$wi
+echo
+$c
+echo End Of Script
